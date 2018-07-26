6d262e68baf9b6c2486ccadd4d7f172ebdcb789a
Florian Grässle <git@holehan.org>
2018-07-26 18:45:28 +0200

    Whitelist selectors generated by hugo

--- themes/termux/postcss.config.js
+++ themes/termux/postcss.config.js
@@ -2,7 +2,17 @@ module.exports = {
   plugins: [
     require('@fullhuman/postcss-purgecss')({
       content: ['themes/termux/layouts/**/*.html'],
-      whitelist: ['highlight', 'language-bash', 'pre', 'code']
+      whitelist: [
+        'highlight',
+        'language-bash',
+        'pre',
+        'code',
+        'content',
+        'h3',
+        'h4',
+        'ul',
+        'li'
+      ]
     }),
     require('autoprefixer')
   ]
